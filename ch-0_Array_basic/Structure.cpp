46ec705904f4dae7e8b274bb995fc62b4aa9387d
harsh <nh1750501@gmail.com>
2025-11-01 14:13:06 +0530

    Added traversing and structures

---
+++ ch-0_Array_basic/Structure.cpp
@@ -0,0 +1,2 @@
+// A structure (often shortened to struct) is a user-defined data type that lets you group together different types of variables under a single name.
+
---
+++ ch-0_Array_basic/traversing.cpp
@@ -0,0 +1,21 @@
+#include<iostream>>
+using namespace std;
+
+
+int main(){
+  int A[5];
+  cout<<"Enter the elements : ";
+   for (int i = 0; i < 5; i++)
+   {
+    cin>>A[i];
+   }
+   cout<< "The Elements Are:"<<endl;
+
+   for (int i = 0; i < 5; i++)
+   {
+    cout<<A[i]<< " ";
+   }
+   cout<<endl;
+    return 0;
+   
+}
