8f67e6861cd8c84efaadaa7ded1e9d8ea5cf137e
harsh <nh1750501@gmail.com>
2025-11-01 21:17:26 +0530

    Added structures

--- ch-0_Array_basic/Structure.cpp
+++ ch-0_Array_basic/Structure.cpp
@@ -1,2 +1,25 @@
 // A structure (often shortened to struct) is a user-defined data type that lets you group together different types of variables under a single name.
 
+
+#include<iostream>
+using namespace std;
+
+struct Rectangle
+{
+  int lenght;
+  int breadth;
+};
+
+int main (){
+  struct Rectangle r1 = {10,5};
+  cout<<sizeof(r1);
+  // dot operstor for accessing memmbers
+  cout<<r1.lenght<<endl;
+
+  cout<<r1.breadth<<endl;;
+  return 0;
+  
+  
+  
+}
+
