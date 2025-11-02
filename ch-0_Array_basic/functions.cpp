b41da657af23f5ed9f954662700b4d3e7f1eced8
harsh <nh1750501@gmail.com>
2025-11-02 19:53:15 +0530

    Added functions

--- ch-0_Array_basic/functions.cpp
+++ ch-0_Array_basic/functions.cpp
@@ -1,16 +1,18 @@
 // integrating all the function in main func. is modular or procedural programming
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-  int a;
-  cin>>a;
-  if (a>=90)
-  {
-    cout<<"Grade A";
-  }
-  else if(a<=34){
-    cout<<"Fail";
-  }
+
+int add(int a, int b)
+{
+  int c;
+  c = a + b;
+  return c;
+}
+int main()
+{
+  int n1, n2;
+  cin >> n1 >> n2;
+  int c = add(n1, n2);
+  cout << "Value is " << c;
   return 0;
-  
 }
