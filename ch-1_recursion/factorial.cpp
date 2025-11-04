735a5dd0e54e7a2db888b59c6f69b1f6a38fb20d
harsh <nh1750501@gmail.com>
2025-11-04 18:38:31 +0530

    Added factorial

---
+++ ch-1_recursion/factorial.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n)
+{
+
+  if (n == 1)
+  {
+    return 1;
+  }
+
+  //  Recursive Step (call itself with a smaller problem)
+  else
+  {
+    return n * factorial(n - 1);
+  }
+}
+
+int main()
+{
+  int a;
+  cout << "Enter the number :";
+  cin >> a;
+  cout << factorial(a) << endl;
+  return 0;
+}
