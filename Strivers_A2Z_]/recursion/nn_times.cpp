b7a18ae0f3a9ee0d189fe22f3fae55edb5d2e190
harsh <nh1750501@gmail.com>
2025-11-05 17:55:37 +0530

    updated code base

--- Strivers_A2Z_]/recursion/nn_times.cpp
+++ Strivers_A2Z_]/recursion/nn_times.cpp
@@ -1,22 +1,25 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void printn(int n){
-  if(n==0){
-      return;
+void printn(int n)
+{
+  if (n == 0)
+  {
+    return;
   }
-  cout<<n<<endl;
-  printn(n-1);
+  cout << n << endl;
+  printn(n - 1);
 }
 
-int main (){
+int main()
+{
   int m;
-  
+
   cout << "Enter a number: ";
   cin >> m;
-  
+
   printn(m);
   cout << endl;
-  
-    return 0;
+
+  return 0;
 }
