357698b8c9c4d57faa9b79d5054922acc4ae0879
harsh <nh1750501@gmail.com>
2025-12-16 16:54:48 +0530

    TErnary

---
+++ ch-0_Array_basic/tempCodeRunnerFile.cpp
@@ -0,0 +1,1 @@
+cout<< "The Elements Are:"<<endl;
---
+++ ch-0_Array_basic/ternary.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main (){
+  int age;
+  cout<<"Enter the age ";
+  cin>>age;
+
+  (age>18)? cout<<"VAlid" : cout<<"Invalid";
+  return 0;
+
+}
