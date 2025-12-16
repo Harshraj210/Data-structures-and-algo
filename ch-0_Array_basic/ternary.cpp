589e238982f9b42762df674d288a91657bd16cb4
harsh <nh1750501@gmail.com>
2025-12-16 16:56:17 +0530

    Added notes

--- ch-0_Array_basic/ternary.cpp
+++ ch-0_Array_basic/ternary.cpp
@@ -5,6 +5,10 @@ int main (){
   cout<<"Enter the age ";
   cin>>age;
 
+  // ? --> ke baad true condition 
+  // : --> ke  baad false condition 
+
+
   (age>18)? cout<<"VAlid" : cout<<"Invalid";
   return 0;
 
