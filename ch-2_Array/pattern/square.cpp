131cd956042ef006dcbd2f521eab07e980c58310
harsh <nh1750501@gmail.com>
2026-01-13 23:24:52 +0530

    updated file

--- ch-2_Array/pattern/square.cpp
+++ ch-2_Array/pattern/square.cpp
@@ -1,27 +1,37 @@
 #include <iostream>
 using namespace std;
-// void printsquare(int n)
-// {
+void printsquare(int n)
+{
 
-//   int i, j;
-//   for (int i = 1; i <= 5; i++)
-//   {
-//     for (int j = 0; j <= 5; j++)
-//     {
-//       cout << "* " << endl;
-//     }
-//   }
-// }
+  for (int i = 1; i <= n; i++)
+  {
+    for (int j = 1; j <= n; j++)
+    {
+      cout << "* ";
+    }
+    cout << endl;
+  }
+}
+void rightinvertedtrinagle(int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < i + 1; j++)
+    {
+      cout << "* ";
+    }
+    cout << endl;
+  }
+}
+void leftinvertedtriangle(int n)
+{
+}
 int main()
 {
-  int n = 1;
-    for (int i = 1; i <= n; i++)
-   {
-      for (int j = 1; j <= n; j++)
-      {
-        cout << "* ";
-    }
-    cout<<endl;
-  }
+  int n = 5;
+  // printsquare(n);
+  rightinvertedtrinagle(5);
+  leftinvertedtriangle();
+
   return 0;
 }
