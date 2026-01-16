80d7595547ab0ef8389eab6115341920614cc048
harsh <nh1750501@gmail.com>
2026-01-17 00:20:09 +0530

    pyramid

---
+++ ch-2_Array/pattern/solid_pyramid.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+void solid_pyramid(int n){
+  for (int i =1;i<=n;i++){
+    for (int j=1;j<=(n-i);j++){
+      cout<<" ";
+    }
+    for (int j=1;j<=(2*i-1);j++){
+      cout<<"* ";
+    }
+    cout<<endl;
+  }
+}
+int main (){
+  solid_pyramid(5);
+  return 0;
+}
