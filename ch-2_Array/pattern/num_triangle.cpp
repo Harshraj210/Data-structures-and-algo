1106f7eb0f403f0a9bbf79730507c010c6b5b539
harsh <nh1750501@gmail.com>
2026-01-17 00:36:33 +0530

    inc triangle number

--- ch-2_Array/pattern/num_triangle.cpp
+++ ch-2_Array/pattern/num_triangle.cpp
@@ -3,13 +3,30 @@ using namespace std;
 void num_tri(int n){
 
   for (int i = 0;i<=n;i++){
-    for (int j = 0;j<i;j++){
+    for (int j = 0;j<=i;j++){
       cout<<i;
+      
     }
     cout<<endl;
   }
 }
+void num_inc_tri(int n){
+  int count =1;
+
+  for (int i = 0;i<=n;i++){
+    for (int j = 0;j<=i;j++){
+      cout<<count<<" ";
+      count++;
+      
+    }
+    cout<<endl;
+  }
+}
+
+
+
 int main (){
-  num_tri(5);
+  // num_tri(5);
+  num_inc_tri(4);
   return 0;
 }
