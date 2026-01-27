63346aa889c7c1ec01e82c2c2557f19988f9506e
harsh <nh1750501@gmail.com>
2026-01-27 11:08:02 +0530

    updated conatiners

---
+++ ch-4_Stl/containers/vector.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int main() {
+    // creation
+    vector<int> marks;
+    // .begin() -->points to starting ele of container
+    // cout << *(marks.begin());
+    // cout << *(marks.end());
+
+    // push_back--> adds ele at first then at back;
+
+    marks.push_back(10);
+    marks.push_back(20);
+    marks.push_back(30);
+    marks.push_back(40);
+    // marks.clear();
+
+    marks.insert(marks.begin(),50);
+    // insert()--> is used to add elements into a container at a specific position.
+    // begin-->iterator pointing to the first element of a container.
+
+    cout << "SIze : " << marks.size() << endl;
+    // pop_back--> removes ele from
+    marks.pop_back();
+    cout << "SIze : " << marks.size() << endl;
+    cout << marks.at(1) << endl;
+    return 0;
+}
