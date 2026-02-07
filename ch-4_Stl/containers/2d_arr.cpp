5c440ce8d722647ee6c2203733c3898c734d09af
harsh <nh1750501@gmail.com>
2026-02-07 20:07:49 +0530

    updated code base

--- ch-4_Stl/containers/2d_arr.cpp
+++ ch-4_Stl/containers/2d_arr.cpp
@@ -5,7 +5,18 @@ using namespace std;
 
 int main() {
     vector<vector<int> > arr(5, vector<int>(5, 0));
+    // 5 rows ,5 cols ,each cell is 0
     // A vector of vectors
 
     // Each inner vector represents one row return 0;
+
+    int totalrows = arr.size();
+    int totalcols = arr[0].size();
+
+    vector<vector<int>> brr(4);
+    brr[0]=vector<int>(4);
+    brr[1]=vector<int>(7);
+    brr[2]=vector<int>(8);
+    brr[3]=vector<int>(5);
+
 }
