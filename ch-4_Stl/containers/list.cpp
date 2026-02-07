967f61f8f10c92e4220988cd9bd7a2b595baef95
harsh <nh1750501@gmail.com>
2026-02-07 20:24:34 +0530

    updated code base

--- ch-4_Stl/containers/list.cpp
+++ ch-4_Stl/containers/list.cpp
@@ -2,3 +2,19 @@
 // doubly linked list
 #include <list>
 using namespace std;
+
+int main() {
+    // creation
+    list<int> mylist;
+    mylist.push_back(10);
+    mylist.push_back(20);
+    mylist.push_back(30);
+    // mylist.push_back(40);
+    mylist.push_front(200);
+    mylist.pop_back();
+    for (int x : mylist) {
+        cout << x << " ";
+    }
+    cout << endl;
+    return 0;
+}
