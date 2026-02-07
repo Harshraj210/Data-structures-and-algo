b758b1dac05a6b3f0ba77f36b6d1da9a9b1083a2
harsh <nh1750501@gmail.com>
2026-02-07 20:16:15 +0530

    traversing 2 d array

--- ch-4_Stl/containers/2d_arr.cpp
+++ ch-4_Stl/containers/2d_arr.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main() {
-    vector<vector<int> > arr(5, vector<int>(5, 0));
+    vector<vector<int>> arr(5, vector<int>(5, 0));
     // 5 rows ,5 cols ,each cell is 0
     // A vector of vectors
 
@@ -14,9 +14,13 @@ int main() {
     int totalcols = arr[0].size();
 
     vector<vector<int>> brr(4);
-    brr[0]=vector<int>(4);
-    brr[1]=vector<int>(7);
-    brr[2]=vector<int>(8);
-    brr[3]=vector<int>(5);
-
+    // jagged arraY
+    brr[0] = vector<int>(4);
+    brr[1] = vector<int>(7);
+    brr[2] = vector<int>(8);
+    brr[3] = vector<int>(5);
+    int totalrowscount = brr.size();
+    for (int i = 0; i < brr.size(); i++) {
+        cout << "Columns in row " << i << " = " << brr[i].size() << endl;
+    }
 }
