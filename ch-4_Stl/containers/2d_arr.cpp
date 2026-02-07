3da7586fffd85eb98d49b2c526b7003b06c48159
harsh <nh1750501@gmail.com>
2026-02-07 20:00:58 +0530

    2d array using vectors

---
+++ ch-4_Stl/containers/2d_arr.cpp
@@ -0,0 +1,11 @@
+// 2d array
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<vector<int> > arr(5, vector<int>(5, 0));
+    // A vector of vectors
+
+    // Each inner vector represents one row return 0;
+}
