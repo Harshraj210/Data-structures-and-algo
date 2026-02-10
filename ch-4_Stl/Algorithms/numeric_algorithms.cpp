2cf6e14f007421f4405819c61672e4fd94b4b869
harsh <nh1750501@gmail.com>
2026-02-11 02:04:30 +0530

    accumulate

--- ch-4_Stl/Algorithms/numeric_algorithms.cpp
+++ ch-4_Stl/Algorithms/numeric_algorithms.cpp
@@ -3,3 +3,15 @@
 #include <algorithm>
 #include <vector>
 using namespace std;
+
+int main() {
+    vector<int> arr(5);
+    arr[0] = 10;
+    arr[1] = 50;
+    arr[2] = 60;
+    arr[3] = 5;
+    int totalsum = accumulate(arr.begin (),arr.end(),0);
+    cout<<totalsum<<" ";
+    
+    return 0;
+}
