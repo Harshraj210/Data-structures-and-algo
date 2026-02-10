e17cc89fc79a3d08be73bdaef0a3bc580f469581
harsh <nh1750501@gmail.com>
2026-02-11 01:40:06 +0530

    added find algo

--- ch-4_Stl/Algorithms/iterator_algorithm.cpp
+++ ch-4_Stl/Algorithms/iterator_algorithm.cpp
@@ -1,5 +1,7 @@
-#include <algorithm>
 #include <iostream>
+// Algorithms are like a collection of pre-written recipes that perform common tasks for you so you don't have to write
+// complex loops from scratch every time.
+#include <algorithm>
 #include <vector>
 using namespace std;
 
@@ -12,6 +14,11 @@ int main() {
     arr[1] = 20;
     arr[2] = 30;
     arr[3] = 40;
-    for_each(arr.begin(), arr.end(), doubleValue);
+    // no ned of writing complex forloop
+    // for_each(arr.begin(), arr.end(), doubleValue);
+
+    int target = 10;
+    auto it = find(arr.begin(), arr.end(), target);
+    cout << *it << " ";
     return 0;
 }
