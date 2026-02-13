e5dec75f65b24b3527250888f029524c3135ef81
harsh <nh1750501@gmail.com>
2026-02-13 21:56:09 +0530

    updated code base

---
+++ ch-4_Stl/functors/custom_priority.cpp
@@ -0,0 +1,45 @@
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class student {
+    // private
+public:
+    int marks;
+    string name;
+
+    student() {
+        marks = 0;
+        name = "";
+    }
+
+    student(int m, string n) {
+        this->marks = m;
+        this->name = n;
+    }
+};
+
+class comparator {
+public:
+// operator overload 
+    bool operator()(student a, student b) {
+        return a.marks < b.marks; 
+    }
+};
+
+int main() {
+
+    priority_queue<student, vector<student>, comparator> pq;
+
+    pq.push(student(99,"Harsh"));
+    pq.push(student(95,"SADSA"));
+    pq.push(student(92,"hhs"));
+
+    cout << pq.top().marks << " " << pq.top().name;
+
+    return 0;
+}
