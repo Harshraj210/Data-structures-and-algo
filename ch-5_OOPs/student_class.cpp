3fef5ea007f258d059884108a60702dea67322ae
harsh <nh1750501@gmail.com>
2026-02-21 12:48:24 +0530

    desturctor

--- ch-5_OOPs/student_class.cpp
+++ ch-5_OOPs/student_class.cpp
@@ -1,19 +1,26 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class Studennt{
-  // public main me acees
-  public:
-  // attributes
+class Studennt {
+    // public main me acees
+public:
+    // attributes
     int marks;
     int nos;
     string name;
     int id;
-    void study(){
-      cout<<"study"<<endl;
-
+    // ctor:default
+    Studennt() {
+        cout < < < "Ctor" << endl;
+    }
+    void study() {
+        cout << "study" << endl;
     }
 
-    void sleep(){
-      cout<<this->name<<"sleep"<<endl;
+    void sleep() {
+        cout << this->name << "sleep" << endl;
+    }
+    // destructor
+    ~Studennt() {
+        cout << "Detor" << endl;
     }
 }
