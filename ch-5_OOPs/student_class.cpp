2746069719291a26b0f1d155cae1693f88dc57b1
harsh <nh1750501@gmail.com>
2026-02-21 13:08:03 +0530

    copy constructor

--- ch-5_OOPs/student_class.cpp
+++ ch-5_OOPs/student_class.cpp
@@ -9,9 +9,23 @@ public:
     string name;
     int id;
     // ctor:default
-    Studennt() {
-        cout < < < "Ctor" << endl;
+    // parameterised const
+    Studennt(int marks, int nos, string name, int id) {
+        cout << "Ctor" << endl;
+        this->marks = marks;
+        this->nos = nos;
+        this->name = name;
+        this->id = id;
     }
+    // pass by refernce
+    Studennt(const Studennt &srcopy) {
+        cout << "Copy Ctor" << endl;
+        this->marks = srcopy.marks;
+        this->nos = srcopy.nos;
+        this->name = srcopy.name;
+        this->id = srcopy.id;
+    }
+    // methods
     void study() {
         cout << "study" << endl;
     }
@@ -20,7 +34,13 @@ public:
         cout << this->name << "sleep" << endl;
     }
     // destructor
+    // function that cleans object when destroyed.
     ~Studennt() {
         cout << "Detor" << endl;
     }
+};
+int main() {
+    Studennt s1(99, 5, "harsh", 1);
+    s1.study();
+    return 0;
 }
