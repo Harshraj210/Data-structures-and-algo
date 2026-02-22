7f314a4b551d33301ff1b5de3bc61e82f33c6dc6
harsh <nh1750501@gmail.com>
2026-02-22 20:54:58 +0530

    updated code base

--- ch-5_OOPs/03_inheritance.cpp
+++ ch-5_OOPs/03_inheritance.cpp
@@ -24,6 +24,7 @@ public:
 class car : public vehicle {
     int doors;
     string transmission;
+
 public:
     car(string _name, string _model, int _tyre, int _doors, string _transmission) : vehicle(_name, _model, _tyre) {
         this->doors = _doors;
@@ -33,10 +34,38 @@ public:
         cout << "ac started " << name << " " << endl;
     }
 };
+class motorcycle : public vehicle {
+    string handlebar;
+    string suspension;
+
+public:
+    motorcycle(string _name, string _model, int _tyre, string _handlebar, string _suspension)
+        : vehicle(_name, _model, _tyre) {
+        this->handlebar = _handlebar;
+        this->suspension = _suspension;
+    }
+
+    void wheelie() {
+        cout << name << " doing wheelie!" << endl;
+    }
+
+    void showSpecs() {
+        cout << "Motorcycle: " << name << " " << model << " " << handlebar << " " << suspension << endl;
+    }
+};
+
 int main() {
-  car a("mercedes","s-class",4,4,"manual");
+    car a("mercedes", "s-class", 4, 4, "manual");
+    // bike call
+    motorcycle m("Hayabhusa", "19", 2, "Clip-on", "Mono-shock");
+
     a.engine_st();
     a.start_ac();
     a.engine_stop();
-  return 0;
+    cout << endl;
+
+    m.engine_st();
+    m.showSpecs();
+    m.wheelie();
+    return 0;
 }
