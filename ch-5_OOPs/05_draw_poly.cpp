78421d7808f721bbb955b32079862c5544c90183
harsh <nh1750501@gmail.com>
2026-02-23 13:54:57 +0530

    updaetd code base'

--- ch-5_OOPs/05_draw_poly.cpp
+++ ch-5_OOPs/05_draw_poly.cpp
@@ -1,10 +1,37 @@
 #include <iostream>
 using namespace std;
 
-class Complex {
-  public:
-    void dra(){
-      cout<<"Generic drawaing"<<endl;
+class shape {
+public:
+    virtual void draw() {  // virtual
+        cout << "Generic drawing" << endl;
     }
 };
-class circle: 
+
+class circle : public shape {
+public:
+    void draw() override {  // override (optional but good)
+        cout << "circle drawing" << endl;
+    }
+};
+
+class rectangle : public shape {
+public:
+    void draw() override {
+        cout << "rectangle drawing" << endl;
+    }
+};
+
+void shape_draw(shape* s) {
+    s->draw();  // runtime decision
+}
+
+int main() {
+    circle c;
+    rectangle r;
+
+    shape_draw(&c);
+    shape_draw(&r);
+
+    return 0;
+}
