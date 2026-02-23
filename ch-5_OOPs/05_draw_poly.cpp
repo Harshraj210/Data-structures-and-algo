f636fb2e9e2442848c33dbecf6a5cbcbb4571acf
harsh <nh1750501@gmail.com>
2026-02-23 13:58:50 +0530

    updated code base

--- ch-5_OOPs/05_draw_poly.cpp
+++ ch-5_OOPs/05_draw_poly.cpp
@@ -17,7 +17,7 @@ public:
 
 class rectangle : public shape {
 public:
-    void draw() override {
+    void draw() override final  { // cannot be overridden further
         cout << "rectangle drawing" << endl;
     }
 };
@@ -27,11 +27,20 @@ void shape_draw(shape* s) {
 }
 
 int main() {
+     shape s;
     circle c;
     rectangle r;
 
-    shape_draw(&c);
+    cout << "Direct calls:" << endl;
+    s.draw();
+    c.draw();
+    r.draw();
+
+    cout << "\nRuntime polymorphism:" << endl;
+    shape_draw(&c);  // circle version
     shape_draw(&r);
 
     return 0;
 }
+// final --> No further overriding allowed
+// virtual --> Call the function based on the real object type at runtime
