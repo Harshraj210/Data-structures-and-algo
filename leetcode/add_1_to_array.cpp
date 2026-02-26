498d8a74397acb2bfbceadfbe068d0051ef1c4ee
harsh <nh1750501@gmail.com>
2026-02-26 14:34:20 +0530

    added1 to array

---
+++ leetcode/add_1_to_array.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for(int i = digits.size()-1; i >= 0; i--) {
+            if(digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        // if all digits are 9
+        digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};
+
+int main() {
+    Solution s;
+    vector<int> digits = {9,9,9};
+    // o--> 1000
+
+    vector<int> ans = s.plusOne(digits);
+
+    for(int x : ans) cout << x << " ";
+}
