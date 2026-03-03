60540f7efb19b6aea8503eae27d95496744496f9
harsh <nh1750501@gmail.com>
2026-03-03 13:15:08 +0530

    stairs

---
+++ leetcode/climbing_stairs.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int climbStairs(int n) {
+
+    if(n <= 2) return n;
+
+    int prev2 = 1;
+    int prev1 = 2;
+    int curr;
+
+    for(int i = 3; i <= n; i++){
+        curr = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = curr;
+    }
+
+    return prev1;
+}
+
+int main() {
+    int n;
+    cout << "Enter : ";
+    cin >> n;
+
+    cout << "Ways to climb: " << climbStairs(n) << endl;
+
+    return 0;
+}
