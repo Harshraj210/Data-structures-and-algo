849993ee290c8ac20ededd272e4cbc98c469c757
harsh <nh1750501@gmail.com>
2026-03-05 12:19:31 +0530

    consequitive.cpp

---
+++ Strivers_A2Z_]/array/consequitive.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int findMaxConsecutiveOnes(vector<int>& nums) {
+
+    int count = 0;
+    int maxCount = 0;
+
+    for(int num : nums) {
+          
+        if(num == 1)
+            count++;
+        else
+            count = 0;
+
+        maxCount = max(maxCount, count);
+    }
+
+    return maxCount;
+}
+
+int main() {
+
+    vector<int> nums = {1,1,0,1,1,1};
+
+    cout << findMaxConsecutiveOnes(nums);
+
+}
