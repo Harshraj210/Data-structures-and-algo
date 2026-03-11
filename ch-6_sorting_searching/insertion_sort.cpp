1f6dcfe2a6ec13a98719284b686608795857a43f
harsh <nh1750501@gmail.com>
2026-03-11 10:06:43 +0530

    updated code base

--- ch-6_sorting_searching/insertion_sort.cpp
+++ ch-6_sorting_searching/insertion_sort.cpp
@@ -2,29 +2,27 @@
 #include <vector>
 using namespace std;
 
-void insertion(vector<int> &v){
-  int n=v.size();
-  // i=0 ignored
-  for (int i = 1;i<n;i++){
-    int key = v[i];
-    int j = i-1;
-    while(j>=0 && v[j]>key){
-      // swapping
-      v[j+1]= v[j];
-      j--;
-
+void insertion(vector<int>& v) {
+    int n = v.size();
+    // i=0 ignored
+    for (int i = 1; i < n; i++) {
+        int key = v[i];
+        // moves backward
+        int j = i - 1;
+        // If previous element is greater than key   shift it to the right
+        while (j >= 0 && v[j] > key) {
+            // swapping
+            v[j + 1] = v[j];
+            j--;
+        }
+        v[j + 1] = key;
     }
-    v[j+1]=key;
-  }
-
 }
-int main (){
-  vector<int> v = {5,4,3,2,1,0};
-  insertion(v);
-  for(int x:v){
-    cout<<x<<" ";
-  }
-  return 0;
-  
-
+int main() {
+    vector<int> v = {5, 4, 3, 2, 1, 0};
+    insertion(v);
+    for (int x : v) {
+        cout << x << " ";
+    }
+    return 0;
 }
