c1ec7b149dfe16cac62e52717c79c423bbfd7610
harsh <nh1750501@gmail.com>
2026-03-11 10:33:20 +0530

    selection

---
+++ ch-6_sorting_searching/selection_sort.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void selectionSort(vector<int> &v){
+
+    int n = v.size();
+
+    for(int i = 0; i < n-1; i++){
+      
+        int minIndex = i;
+
+        for(int j = i+1; j < n; j++){
+
+            if(v[j] < v[minIndex]){
+                minIndex = j;
+            }
+
+        }
+
+        swap(v[i], v[minIndex]);
+    }
+}
+
+int main(){
+
+    vector<int> v = {5,3,8,4,2};
+
+    selectionSort(v);
+
+    for(int x : v){
+        cout << x << " ";
+    }
+
+}
