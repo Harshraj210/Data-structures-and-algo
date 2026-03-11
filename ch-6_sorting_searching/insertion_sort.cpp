a11a240d2d61d4fe9a34afdacdb5e7fc43ae7541
harsh <nh1750501@gmail.com>
2026-03-11 10:03:38 +0530

    insertion sort

--- ch-6_sorting_searching/insertion_sort.cpp
+++ ch-6_sorting_searching/insertion_sort.cpp
@@ -6,6 +6,23 @@ void insertion(vector<int> &v){
   int n=v.size();
   for (int i = 1;i<n;i++){
     int key = v[i];
+    int j = i-1;
+    while(j>=0 && v[j]>key){
+      v[j+1]= v[j];
+      j--;
+
+    }
+    v[j+1]=key;
   }
+
+}
+int main (){
+  vector<int> v = {5,4,3,2,1,0};
+  insertion(v);
+  for(int x:v){
+    cout<<x<<" ";
+  }
+  return 0;
+  
 
 }
