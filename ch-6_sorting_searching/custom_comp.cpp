1264945ae48c4f667a425a4aa20799b797ea3bf0
harsh <nh1750501@gmail.com>
2026-03-12 12:54:29 +0530

    updated code base

--- ch-6_sorting_searching/custom_comp.cpp
+++ ch-6_sorting_searching/custom_comp.cpp
@@ -3,13 +3,13 @@
 #include <vector>
 
 using namespace std;
-bool mycomp(int& a, int& b) {
+bool mycomp(int &a, int &b) {
     return a > b;
 }
 int main() {
     vector<int> v = {3, 2, 1, 6, 7, 6};
 
-    sort(v.begin(), v.end());
+    sort(v.begin(), v.end(), mycomp);
     for (int num : v) {
         cout << num << endl;
     }
