5a5370015ab02d61559f826f56497e17051a3dc3
harsh <nh1750501@gmail.com>
2026-03-12 12:53:06 +0530

    custom_comparator

---
+++ ch-6_sorting_searching/custom_comp.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+bool mycomp(int& a, int& b) {
+    return a > b;
+}
+int main() {
+    vector<int> v = {3, 2, 1, 6, 7, 6};
+
+    sort(v.begin(), v.end());
+    for (int num : v) {
+        cout << num << endl;
+    }
+    return 0;
+}
