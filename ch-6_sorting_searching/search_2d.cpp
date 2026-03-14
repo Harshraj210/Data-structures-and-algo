e2e8e1623fa18703e05464cc12de2829a7c8dc56
harsh <nh1750501@gmail.com>
2026-03-14 19:25:23 +0530

    searh 2d

---
+++ ch-6_sorting_searching/search_2d.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    int rows = matrix.size();
+    int cols = matrix[0].size();
+
+    int start = 0;
+    int end = rows * cols - 1;
+
+    while (start <= end) {
+        int mid = start + (end - start) / 2;
+
+        int row = mid / cols;
+        int col = mid % cols;
+
+        if (matrix[row][col] == target)
+            return true;
+
+        else if (matrix[row][col] < target)
+            start = mid + 1;
+
+        else
+            end = mid - 1;
+    }
+
+    return false;
+}
+
+int main() {
+    vector<vector<int>> matrix = {{1, 4, 7, 11}, {2, 5, 8, 12}, {3, 6, 9, 16}};
+
+    cout << searchMatrix(matrix, 5);
+}
