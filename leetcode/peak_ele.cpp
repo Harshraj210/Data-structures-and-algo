87ba4e4cd721444618ecb707f173db46c396ffbe
harsh <nh1750501@gmail.com>
2026-03-16 12:29:56 +0530

    peak_ele

---
+++ leetcode/peak_ele.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int findPeakElement(vector<int>& nums) {
+
+    int n = nums.size();
+
+    if(n == 1) return 0;
+
+    if(nums[0] > nums[1]) return 0;
+    if(nums[n-1] > nums[n-2]) return n-1;
+
+    int s = 1, e = n-2;
+
+    while(s <= e){
+
+        int mid = s + (e - s) / 2;
+
+        if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1])
+            return mid;
+
+        else if(nums[mid] < nums[mid+1])
+            s = mid + 1;
+
+        else
+            e = mid - 1;
+    }
+
+    return -1;
+}
+
+int main() {
+
+    vector<int> nums = {1,2,3,1};
+
+    int peakIndex = findPeakElement(nums);
+
+    cout << "Peak index: " << peakIndex << endl;
+
+    return 0;
+}
