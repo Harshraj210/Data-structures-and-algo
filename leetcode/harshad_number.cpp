9b5738848040a6000dca54e8fd7eafec9cf0cff2
harsh <nh1750501@gmail.com>
2026-03-17 19:51:13 +0530

    harshad num

---
+++ leetcode/harshad_number.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int sumOfTheDigitsOfHarshadNumber(int x) {
+
+    int sum = 0;
+    int temp = x;
+
+    while(temp > 0){
+        sum += temp % 10;
+        temp /= 10;
+    }
+
+    if(x % sum == 0)
+        return sum;
+
+    return -1;
+}
+
+int main() {
+
+    int x;
+    cin >> x;
+
+    cout << sumOfTheDigitsOfHarshadNumber(x);
+
+    return 0;
+}
