d337f2f67997aeb96b9cd55c08b6a444d71f8427
harsh <nh1750501@gmail.com>
2026-04-08 18:31:23 +0530

    updated code base

--- ch-7_strings/01_basic.cpp
+++ ch-7_strings/01_basic.cpp
@@ -1,10 +1,19 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-  char arr[50];
-  cout<<"Enter ";
-  // getline --> reaads strings after space
-  cin.getline(arr, 50);
-  return 0;
+int getlen (char arr[],int len){
+  int count =0;
+  while(arr[count] !='\0'){
+    count ++;
+  }
+  return count;
+}
+
+int main() {
+    char arr[50];
+    cout << "Enter ";
+    // getline --> reaads strings after space
+    cin.getline(arr, 50);
+    cout<<"lenght  of arr "<<getlen(arr,50);
+    return 0;
 }
