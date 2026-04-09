a000d08788debbc0dcd4bc13e6d8cd7672622888
harsh <nh1750501@gmail.com>
2026-04-09 22:23:11 +0530

    reverse_string

---
+++ ch-7_strings/reverse_string.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <cctype>   // used bcz for alpha
+using namespace std;
+
+string reverseOnlyLetters(const string& s) {
+
+    string str = s;   
+
+    int start = 0;
+    int end = str.length() - 1;
+
+    while(start < end){
+
+        if(!isalpha(str[start])) {
+            start++;
+        }
+        else if(!isalpha(str[end])) {
+            end--;
+        }
+        else {
+            swap(str[start], str[end]);
+            start++;
+            end--;
+        }
+    }
+
+    return str;
+}
+
+int main() {
+
+    string s;
+    cout << "Enter string: ";
+    cin >> s;
+
+    string result = reverseOnlyLetters(s);
+
+    cout << "Output: " << result << endl;
+
+    return 0;
+}
