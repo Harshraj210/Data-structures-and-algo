deea33e0722b566e54efddf51ad2ed96af476b20
harsh <nh1750501@gmail.com>
2026-04-18 15:59:09 +0530

    roti parata

---
+++ ch-6_sorting_searching/roti_paratha.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+// Check if possible to cook p pratas in  time
+bool canCook(vector<int>& ranks, int p, int time) {
+  
+  int total =0; // initial parata
+  for(int i = 0; i < ranks.size(); i++){
+    int R = ranks[i];
+        int t = 0;
+        int j = 1;
+  }
+}
+
+    
+
+    
