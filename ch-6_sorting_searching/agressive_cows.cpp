7a0f3684210a2a8312d5d6fc039a0cea9ed57303
harsh <nh1750501@gmail.com>
2026-04-18 15:32:28 +0530

    agreesive cows

--- ch-6_sorting_searching/agressive_cows.cpp
+++ ch-6_sorting_searching/agressive_cows.cpp
@@ -1,32 +1,65 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
-bool canplace(vector<int>& stall,int k,int dist){
-  // first cow
-  int count =1;
-  
-  int last = stall[0];
-  for(int i =1;i<stall.size();i++){
+bool canplace(vector<int>& stall, int k, int dist) {
+    // first cow
+    int count = 1;
 
-    if(stall[i]-last>=dist){
-      count++;
-      last=stall[i];
+    int last = stall[0];
+    for (int i = 1; i < stall.size(); i++) {
+        if (stall[i] - last >= dist) {
+            count++;
+            last = stall[i];
+        }
+        // placced all cows
+        if (count >= k) {
+            return true;
+        }
     }
-    // placced all cows
-    if(count>=k){
-      return true;
-    }
-  }
-  return false;
+    return false;
 }
-int agreesivecows(vector<int>& stalls, int k){
-  sort(stalls.begin(), stalls.end());
+int aggressiveCows(vector<int>& stalls, int k) {
+    sort(stalls.begin(), stalls.end());
 
     int low = 1;
+    // largest distance
     int high = stalls.back() - stalls.front();
     int ans = 0;
-}
+    // binary search
+    while (low <= high) {
+        int mid = (low + high) / 2;
 
+        if (canplace(stalls, k, mid)) {
+            ans = mid;
+            low = mid + 1;  // try bigger distance
+        } else {
+            high = mid - 1;  // reduce distance
+        }
+    }
+    return ans;
+}
+int main() {
+    int n, k;
+
+    cout << "Enter number of stalls: ";
+    cin >> n;
+
+    vector<int> stalls(n);
+
+    cout << "Enter stall positions:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> stalls[i];
+    }
+
+    cout << "Enter number of cows: ";
+    cin >> k;
+
+    int result = aggressiveCows(stalls, k);
+
+    cout << "Maximum minimum distance  " << result << endl;
+
+    return 0;
+}
