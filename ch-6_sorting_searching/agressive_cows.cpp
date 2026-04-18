f878b599642af8aa16f37bcf9d292bb37b9d8b3a
harsh <nh1750501@gmail.com>
2026-04-18 15:26:52 +0530

    updated code base

--- ch-6_sorting_searching/agressive_cows.cpp
+++ ch-6_sorting_searching/agressive_cows.cpp
@@ -7,11 +7,17 @@ using namespace std;
 bool canplace(vector<int>& stall,int k,int dist){
   // first cow
   int count =1;
+  
   int last = stall[0];
   for(int i =1;i<stall.size();i++){
+
     if(stall[i]-last>=dist){
       count++;
       last=stall[i];
+    }
+    // placced all cows
+    if(count>=k){
+      return true;
     }
   }
 }
