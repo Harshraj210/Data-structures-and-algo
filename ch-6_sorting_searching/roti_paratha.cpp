57bb002cc9053fad8831a3d6a225483b9e2c0e39
harsh <nh1750501@gmail.com>
2026-04-18 16:05:38 +0530

    updated code base

--- ch-6_sorting_searching/roti_paratha.cpp
+++ ch-6_sorting_searching/roti_paratha.cpp
@@ -10,8 +10,26 @@ bool canCook(vector<int>& ranks, int p, int time) {
     int R = ranks[i];
         int t = 0;
         int j = 1;
+        // count  paratas  cook can make
+        while(true) {
+            t += j * R;
+            if(t > time) break;
+            total++;
+            j++;
+        }
+
+        if(total >= p) return true;
+    }
+
+    return false;
   }
-}
+  int minTimeToCookPratas(int p, vector<int>& ranks){
+    int low = 0;
+
+    int maxRank = *min_element(ranks.begin(), ranks.end());
+    int high = maxRank * (p * (p + 1)) / 2;
+  }
+
 
     
 
