b7485abb30e8a058986b26417926c1b5d6dbb00c
harsh <nh1750501@gmail.com>
2026-04-28 16:49:09 +0530

    sqrt method

--- ch-8_pointers/basic_math.cpp
+++ ch-8_pointers/basic_math.cpp
@@ -1,4 +1,5 @@
 // naive approach --> basic method cheks all no from 2 to n-1
+// tc = o(n^2)
 bool isPrime(int n) {
     if(n <= 1) return false;
 
@@ -9,4 +10,14 @@ bool isPrime(int n) {
     return true;
 }
 
-// 
+//  sqrt method --> One factor is always ≤ √n
+// tc o(√nn)
+bool isPrime(int n) {
+    if(n <= 1) return false;
+
+    for(int i = 2; i * i <= n; i++) {
+        if(n % i == 0)
+            return false;
+    }
+    return true;
+}
