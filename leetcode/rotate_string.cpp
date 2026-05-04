f7d8bfc44be23cab2690564405fee6ee85301b09
harsh <nh1750501@gmail.com>
2026-05-04 21:23:47 +0530

    rotate_string

---
+++ leetcode/rotate_string.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+
+        
+        if(s.size() != goal.size()) return false;
+
+        //  double the string
+        string temp = s + s;
+
+        // check if goal exists
+        if(temp.find(goal) != string::npos) {
+            return true;
+        }
+
+        return false;
+    }
+};
+
+int main() {
+    Solution obj;
+
+    string s = "abcde";
+    string goal = "cdeab";
+
+    if(obj.rotateString(s, goal)) {
+        cout << "True" << endl;
+    } else {
+        cout << "False" << endl;
+    }
+
+    return 0;
+}
