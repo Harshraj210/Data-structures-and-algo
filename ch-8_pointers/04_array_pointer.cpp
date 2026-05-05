4201df64373957544427db10204378c0109ea8c1
harsh <nh1750501@gmail.com>
2026-05-05 19:04:21 +0530

    updaettd code base

--- ch-8_pointers/04_array_pointer.cpp
+++ ch-8_pointers/04_array_pointer.cpp
@@ -1,11 +1,15 @@
 // array where each element is a pointer
-// int a = 10, b = 20, c = 30;
+#include<iostream>
+using namespace std;
+int main (){
+  int a = 10, b = 20, c = 30;
 
-// int* arr[3];
+int* arr[3];
 
-// arr[0] = &a;
-// arr[1] = &b;
-// arr[2] = &c;
+arr[0] = &a;
+arr[1] = &b;
+arr[2] = &c;
 
-// cout << *arr[0]; // 10
-// cout << *arr[1]; // 20
+cout << *arr[0]; // 10
+cout << *arr[1]; // 20
+}
