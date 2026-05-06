136609eb803a88e8b9005931901969339245ff17
harsh <nh1750501@gmail.com>
2026-05-06 20:47:33 +0530

    updated code base

--- ch-8_pointers/06_optimising_erasthothenes.cpp
+++ ch-8_pointers/06_optimising_erasthothenes.cpp
@@ -1,2 +1,29 @@
-// sieve of erasthothenes --> for findding thew mulprime no. and we mark the multiples of prime no. as false till it's half
-// suppose we  have to mark 4 but it is already marked bcz it is multiple of 2
+// sieve of erasthothenes --> for findding thew mulprime no. and we mark the multiples of prime no. as false till it's
+// half suppose we  have to mark 4 but it is already marked bcz it is multiple of 2
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<bool> Sieve(int n) {
+    vector<bool> seive(n + 1, true);
+    for (int i = 2; i <= n; i++) {
+        if (seive[i] == true) {
+            int j = i * 2;
+            while (j <= n) {
+                seive[j] = false;
+                j += i;
+            }
+        }
+    }
+    return seive;
+}
+int main() {
+    vector<bool> sieve = Sieve(25);
+    for (int i = 0; i <= 25; i++) {
+        if (sieve[i]) {
+            cout << i << " ";
+        }
+    }
+    return 0;
+}
