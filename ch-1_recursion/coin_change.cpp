c004240a4d5f175101c7171d65f4d7b7adff2c99
harsh <nh1750501@gmail.com>
2026-05-13 22:14:26 +0530

    coin

---
+++ ch-1_recursion/coin_change.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int solve(int amount, vector<int>& coins, int index) {
+        if (amount == 0) {
+            return 1;
+        }
+
+        if (amount < 0) {
+            return 0;
+        }
+
+        if (index >= coins.size()) {
+            return 0;
+        }
+
+        int include = solve(amount - coins[index], coins, index);
+
+        int exclude = solve(amount, coins, index + 1);
+
+        return include + exclude;
+    }
+
+    int change(int amount, vector<int>& coins) {
+        return solve(amount, coins, 0);
+    }
+};
+
+int main() {
+    Solution obj;
+
+    vector<int> coins = {1, 2, 5};
+
+    int amount = 5;
+
+    cout << obj.change(amount, coins);
+
+    return 0;
+}
